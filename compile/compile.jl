6639744895c2851e506c6c070da43b9568bb16e4
Oriol Colomes <oriol.colomes@gmail.com>
2023-01-03 10:35:08 +0100

    changes in compile.jl

--- compile/compile.jl
+++ compile/compile.jl
@@ -1,4 +1,4 @@
 using PackageCompiler
 create_sysimage(:CNN_NS,
-  sysimage_path=joinpath(@__DIR__,"..","CNN_NS.so"),
+  sysimage_path=joinpath(@__DIR__,"..","CNN_NS_parallel.so"),
   precompile_execution_file=joinpath(@__DIR__,"warmup.jl"))
