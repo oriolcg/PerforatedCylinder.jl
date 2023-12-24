126f744d343f47ec11c8b00987edd867a05a28af
Oriol Colomes <oriol.colomes@gmail.com>
2023-12-24 21:19:47 +0100

    coarse and fine meshes scripts

--- analysis/generate_fine_meshes.jl
+++ analysis/generate_fine_meshes.jl
@@ -8,7 +8,7 @@ perf_cases = 3:30
 porosities = 0.3:0.02:0.7
 alphas = [0.0]#:15.0/(nalpha-1):15.0
 
-ENV["PerforatedCylinder_MESHES"] = "./data/meshes/"
+ENV["PerforatedCylinder_MESHES"] = "./data/fine_meshes/"
 for num_perforations in perf_cases
   for β in porosities
     for α in alphas
@@ -16,8 +16,9 @@ for num_perforations in perf_cases
       PerforatedCylinder.create_mesh(
         L = 7, Cx = 2, t = 0.1,
         num_perforations = num_perforations, β = β, α = α,
-        h_coarse=0.5, h_fine=0.03, dxRight=5,
-        decay_factor_right=2, decay_exponent_right=0.5)
+        h_coarse=0.4, h_fine=0.01, dxRight=5, dxLeft=0.5,
+        decay_factor_right=10, decay_exponent_right=0.3,
+        decay_factor_left=10, decay_exponent_left=0.6)
     end
   end
 end
